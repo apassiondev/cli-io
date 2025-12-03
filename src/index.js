016adc122a86c45fc6e19207c14fddf2102ba5ee
Andrea Ho <anhhothai.iamcoder@gmail.com>
2025-12-03 20:07:10 +0700

    feat: Accept entries and append them to the pre-defined file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-import { input } from "@inquirer/prompts";
+import { input, confirm } from "@inquirer/prompts";
+import { appendFileSync } from "fs";
 
-(async () => {
-  const answer = await input({ message: "Enter your name" });
-  console.info(answer);
-})();
+const filePath = "./data/resources.csv";
+
+class ResourceEntry {
+  constructor(title = "", url = "") {
+    this.title = title;
+    this.url = url;
+  }
+
+  save() {
+    const content = `${this.title},${this.url}\n`;
+    try {
+      appendFileSync(filePath, content);
+      console.info(`Data "${this.title}" saved!`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+const app = async () => {
+  let shouldContinue = true;
+
+  while (shouldContinue) {
+    try {
+      const title = await input({ message: "Title: " });
+      const url = await input({ message: "URL: " });
+
+      const resource = new ResourceEntry(title, url);
+      resource.save();
+    } catch (error) {
+      console.error(error);
+    }
+
+    shouldContinue = await confirm({ message: "Continue?" });
+
+    if (!shouldContinue) {
+      console.info("👋 Bye!");
+      break;
+    } else {
+      console.log("----- Your Next Entry -----");
+    }
+  }
+};
+
+app();
