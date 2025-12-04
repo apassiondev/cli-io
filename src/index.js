1dc0265326ab73494e26174fa6f01abf33378470
Andrea Ho <anhhothai.iamcoder@gmail.com>
2025-12-04 09:02:29 +0700

    fix: Fix the issue of not showing the CSV headers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import { confirm, input } from "@inquirer/prompts";
 import { createObjectCsvWriter } from "csv-writer";
+import fs from "fs";
 
 const filePath = "./data/resources.csv";
 
+const isFileExisted = fs.existsSync(filePath);
+
 const csvWriter = createObjectCsvWriter({
   path: filePath,
-  append: true,
+  append: isFileExisted,
   header: [
-    { id: "title", title: "TITLE" },
+    { id: "name", title: "NAME" },
     { id: "url", title: "URL" },
   ],
 });
 
 class ResourceEntry {
-  constructor(title = "", url = "") {
-    this.title = title;
+  constructor(name = "", url = "") {
+    this.name = name;
     this.url = url;
   }
 
   async save() {
     try {
-      const { title, url } = this;
-      await csvWriter.writeRecords([{ title, url }]);
+      const { name, url } = this;
+      await csvWriter.writeRecords([{ name, url }]);
     } catch (error) {
       throw new Error(error);
     }
@@ -33,13 +36,13 @@ const app = async () => {
 
   while (shouldContinue) {
     try {
-      const title = await input({ message: "Title: " });
+      const name = await input({ message: "Title: " });
       const url = await input({ message: "URL: " });
 
-      const resource = new ResourceEntry(title, url);
-      resource.save();
+      const resource = new ResourceEntry(name, url);
+      await resource.save();
 
-      console.log(`"${title}" saved!`);
+      console.log(`"${name}" saved!`);
     } catch (error) {
       console.error("Error saving content:", error);
     }
